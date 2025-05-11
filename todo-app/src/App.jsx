ef5238136a355ff8fa31f0b33e78ad4cf968c0a6
DIPSHA <dipshadas4@gmail.com>
2025-05-11 11:52:41 +0530

    Working tabs

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,25 +1,25 @@
 // a component is a function which returns jsx nd jsx is html thaT HAS JS WRITTEN DIREctly inside it 
-import {Header} from "./components/Header"
-import {Tabs} from "./components/Tabs"
-import {TodoInput} from "./components/TodoInput"
-import {TodoList} from "./components/TodoList"
+import { Header } from "./components/Header"
+import { Tabs } from "./components/Tabs"
+import { TodoInput } from "./components/TodoInput"
+import { TodoList } from "./components/TodoList"
 
 
 function App() {
   const todos = [
-    {input : 'Hello! Add your first todo !' , complete: true},
-    {input : 'Get the groceries !' , complete: false},
-    {input : 'Learn how to web design' , complete: false},
-    {input : 'Say hi to gran gran' , complete: true}
+    { input: 'Hello! Add your first todo !', complete: true },
+    { input: 'Get the groceries !', complete: false },
+    { input: 'Learn how to web design', complete: false },
+    { input: 'Say hi to gran gran', complete: true }
   ]
 
   return ( //react fragment <> where we can wrap our code
-     <> 
+    <>
       <Header todos={todos} />
       <Tabs todos={todos} />
       <TodoList todos={todos} />
       <TodoInput />
-      
+
     </>
   )
 }
--- todo-app/src/components/Tabs.jsx
+++ todo-app/src/components/Tabs.jsx
@@ -1,14 +1,19 @@
-export function Tabs() {
-    const tabs = ['All', 'Open' , 'Completed']
-    return (
-      <nav className="tab-container">
-        {tabs.map((tab , tabIndex) => {
-              return (
-                <button key={tabIndex} className="tab-button">
-                  <h4>{tab} <span>(0)</span></h4>
-                </button>
-              )
-        })}
-      </nav>
-    )
+export function Tabs(props) { // telling Tabs function to receive the properties so we are giving it in parameters
+  const { todos } = props // destructuring otr giving the properties to the constant todos
+
+  const tabs = ['All', 'Open', 'Completed']
+  return (
+    <nav className="tab-container">
+      {tabs.map((tab, tabIndex) => {
+        const numOfTasks = tab === 'All' ? todos.length
+          : tab === 'Open' ?
+            todos.filter(val => !val.complete).length : todos.filter(val => val.complete).length
+        return (
+          <button key={tabIndex} className="tab-button">
+            <h4>{tab} <span>({numOfTasks})</span></h4>
+          </button>
+        )
+      })}
+    </nav>
+  )
 }
