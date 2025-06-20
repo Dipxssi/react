d10ceedb613a2cc0bf63d006a802d5f68a14c852
DIPSHA <dipshadas4@gmail.com>
2025-06-20 13:48:20 +0530

    Why React

---
+++ whyReact/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+  return(
+   <header className="header">
+    <nav className="nav-bar">
+    <img src="react-black.png" alt="react-logo" className="image"></img>
+    <span>ReactFacts</span>
+    </nav>
+   </header>
+  )
+}
