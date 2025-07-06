7e7d6ad83ed90eddd397f288b705ab17d5df5f79
DIPSHA <dipshadas4@gmail.com>
2025-07-06 19:09:20 +0530

    Rendering data using useStates

--- YourPersonalChef/src/components/Main.jsx
+++ YourPersonalChef/src/components/Main.jsx
@@ -1,14 +1,35 @@
+import React from "react"
 export default function Main(){
+
+const [ingredients , setIngredients]=React.useState([])
+
+ const ingredientsListItems = ingredients.map(ingredient =>(
+  <li key={ingredient}>{ingredient}</li>
+ ))
+
+
+ function handleSubmit(event){
+  event.preventDefault()
+  const formData = new FormData(event.currentTarget)
+  const newIngredient = formData.get("ingredient")//form name here 
+  setIngredients(prevIngredient=> ( [...prevIngredient,newIngredient]))
+ }
+
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input 
         type = "text"
         placeholder="e.g. salt"
         aria-label="Add ingredient"
+        name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>
+      {ingredientsListItems}
+      </ul>
     </main>
   )
 }
