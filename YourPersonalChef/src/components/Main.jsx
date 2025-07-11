e5e0d930f7f45e87c77dbeb2c200d2ecf23bd41c
DIPSHA <dipshadas4@gmail.com>
2025-07-11 22:40:36 +0530

    Final rendering with condition to get recipe button

--- YourPersonalChef/src/components/Main.jsx
+++ YourPersonalChef/src/components/Main.jsx
@@ -29,6 +29,7 @@ export default function Main() {
       <section>
         <h2>Ingredients on hand:</h2>
         <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+        {ingredients.length > 3 && 
         <div className="get-recipe-container">
           <div>
             <h3>Ready for a recipe?</h3>
@@ -36,6 +37,7 @@ export default function Main() {
           </div>
           <button>Get a recipe</button>
         </div>
+        }
       </section>
       )}
     </main>
