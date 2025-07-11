44ed76c76b267a14b92fc7e711666da04b8f60c5
DIPSHA <dipshadas4@gmail.com>
2025-07-11 21:57:32 +0530

    Rendering ingredients

--- YourPersonalChef/src/components/Main.jsx
+++ YourPersonalChef/src/components/Main.jsx
@@ -1,33 +1,43 @@
 import React from "react"
-export default function Main(){
+export default function Main() {
 
-const [ingredients , setIngredients]=React.useState([])
+  const [ingredients, setIngredients] = React.useState([])
 
- const ingredientsListItems = ingredients.map(ingredient =>(
-  <li key={ingredient}>{ingredient}</li>
- ))
+  const ingredientsListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+  ))
 
 
- function addIngredient(formData){
-  const newIngredient = formData.get("ingredient")//form name here 
-  setIngredients(prevIngredient=> ( [...prevIngredient,newIngredient]))
- }
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevIngredient => ([...prevIngredient, newIngredient]))
+  }
 
 
   return (
     <main>
-      <form action={addIngredient}className="add-ingredient-form" >
-        <input 
-        type = "text"
-        placeholder="e.g. salt"
-        aria-label="Add ingredient"
-        name="ingredient"
+      <form action={addIngredient} className="add-ingredient-form" >
+        <input
+          type="text"
+          placeholder="e.g. salt"
+          aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
-      <ul>
-      {ingredientsListItems}
-      </ul>
+      {ingredients.length > 0 && (
+      <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button>Get a recipe</button>
+        </div>
+      </section>
+      )}
     </main>
   )
 }
