640929f450c42f44c6be8599df7e4f4c645dafc9
DIPSHA <dipshadas4@gmail.com>
2025-07-20 15:03:35 +0530

    Added scrollIntoView in YPC

--- YourPersonalChef/src/components/Main.jsx
+++ YourPersonalChef/src/components/Main.jsx
@@ -1,16 +1,25 @@
 import React from "react"
 import Recipe from "./Recipe.jsx"
 import IngredientList from "./IngredientList.jsx"
-import {getRecipeFromMistral } from "../ai.js"
+import { getRecipeFromMistral } from "../ai.js"
 
 export default function Main() {
 
   const [ingredients, setIngredients] = React.useState([])
 
   const [recipe, setRecipe] = React.useState("")
+
+  const recipeSection = React.useRef(null)
   
-  async function getRecipe(){
-    const  recipeMarkdown =  await getRecipeFromMistral(ingredients)
+  React.useEffect(() => {
+    if(recipe !=="" && recipeSection.current !== null){
+      recipeSection.current.scrollIntoView({behavior:"smooth"})
+    } 
+  },[recipe])
+
+
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromMistral(ingredients)
     setRecipe(recipeMarkdown)
   }
 
@@ -32,8 +41,14 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      {ingredients.length > 0 && <IngredientList ingredients={ingredients} getRecipe={getRecipe}/> }
-      {recipe && <Recipe recipe={recipe}/>}
+      {ingredients.length > 0 &&
+        <IngredientList
+          ref={recipeSection}
+          ingredients={ingredients}
+          getRecipe={getRecipe} />
+      }
+
+      {recipe && <Recipe recipe={recipe} />}
     </main>
   )
 }
